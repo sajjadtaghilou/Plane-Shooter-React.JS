c1758b0cd3ac04b40f0077bc7e0e6e5d2970b9b8
Sajjad Taghilou <sajjad.taghilou@outlook.com>
2019-10-03 12:24:22 +0300

    color

--- src/components/GameStart.js
+++ src/components/GameStart.js
@@ -57,7 +57,7 @@ export default class GameStart extends React.Component {
                 <Header />
                 <div className="content" >
                     <form onSubmit={this.submit.bind(this)}>
-                        <TextField className = "textfield"
+                        <TextField
                             floatingLabelText="Name"
                             floatingLabelStyle={styles.floatingLabelStyle}
                             floatingLabelFocusStyle={styles.floatingLabelFocusStyle}
